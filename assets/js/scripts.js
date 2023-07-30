c542fdd47acd716726b40307161d72751e665576
varma79 <135851281+varma79@users.noreply.github.com>
2023-07-29 23:18:45 -0500

    Updated landing page

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,17 +1,46 @@
-var modal = document.getElementById("contactModal");
-var btn = document.getElementById("contactUs");
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-  modal.style.display = "block";
+function displayLoginModal() {
+  document.getElementById('loginModal').style.display = 'block';
 }
 
-span.onclick = function() {
-  modal.style.display = "none";
+function closeLoginModal() {
+  document.getElementById('loginModal').style.display = 'none';
 }
 
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function displaySignUpModal() {
+  document.getElementById('signUpModal').style.display = 'block';
+}
+
+function closeSignUpModal() {
+  document.getElementById('signUpModal').style.display = 'none';
+  clearFormFields(document.querySelector('#signUpModal .sign-up-form'));
+}
+
+function displayThankYouMessage() {
+  alert("Thank you for submitting. We will respond to you soon.");
+  closeSignUpModal();
+}
+
+function clearFormFields(form) {
+  var inputs = form.getElementsByTagName('input');
+  for (var i = 0; i < inputs.length; i++) {
+    inputs[i].value = '';
   }
+}
+
+window.addEventListener('click', function(event) {
+  if (event.target == document.getElementById('loginModal')) {
+    closeLoginModal();
+  } else if (event.target == document.getElementById('signUpModal')) {
+    closeSignUpModal();
+  } else if (event.target == document.getElementById('contactModal')) {
+    document.getElementById('contactModal').style.display = 'none';
+  }
+});
+
+function displayContactModal() {
+  document.getElementById('contactModal').style.display = 'block';
+}
+
+function closeContactModal() {
+  document.getElementById('contactModal').style.display = 'none';
 }
